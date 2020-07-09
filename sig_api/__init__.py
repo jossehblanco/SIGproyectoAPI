b5d9a3fddc27bd6983f4670b01cace186af89966
jossehblanco <jossehblanco@gmail.com>
2020-07-09 03:14:49 -0600

    Implementando conexión con base de datos

--- sig_api/__init__.py
+++ sig_api/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from models import db, Circle_curated_api
+import json
 
 app = Flask(__name__)
 
@@ -13,13 +15,31 @@ welcome_messages = [
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['DEBUG'] = True
+    POSTGRES = {
+    'user': 'ugkqsbelwstbad',
+    'pw': '22f3336ee10a38155a9140a7e56a4ce378ab17110d6179753d98b27f36b09796',
+    'db': 'dctkv6bissui9r',
+    'host': 'ec2-52-72-65-76.compute-1.amazonaws.com',
+    'port': '5432',
+    }
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ugkqsbelwstbad:22f3336ee10a38155a9140a7e56a4ce378ab17110d6179753d98b27f36b09796@ec2-52-72-65-76.compute-1.amazonaws.com:5432/dctkv6bissui9r' % POSTGRES
+
+    db.init_app(app)
         
-    @app.route('/testget', methods=['GET'])
+    @app.route('/circles', methods=['GET'])
     def welcome():
-        return jsonify(welcome_messages)
+        circles = Circle_curated_api.query.all()
+        lcircles = []
+        for item in circles:
+            lcircles.append(item.tojson())
+        return jsonify(lcircles)
 
     @app.route('/', methods=['GET'])
     def root():
         return "SIG SUPER PODEROSO"
+    app.config['CONFIG'] = True
     return app
     
+create_app().run()
